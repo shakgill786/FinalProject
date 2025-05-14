9d02ec82ae1bfd6ab3bda7a00ebba2a11d9fa832
shakgill786 <sgillani1@gmail.com>
2025-05-14 10:35:39 -0400

    Fix FK in questions to point to correct schema

--- migrations/versions/fd1d1c969e26_initial_clean_migration_with_fk_fix.py
+++ migrations/versions/fd1d1c969e26_initial_clean_migration_with_fk_fix.py
@@ -78,7 +78,7 @@ def upgrade():
     sa.Column('options', sa.PickleType(), nullable=False),
     sa.Column('answer', sa.String(length=100), nullable=False),
     sa.Column('quiz_id', sa.Integer(), nullable=False),
-    sa.ForeignKeyConstraint(['quiz_id'], ['quizzes.id'], ),
+    sa.ForeignKeyConstraint(['quiz_id'], ['your_schema.quizzes.id']),
     sa.PrimaryKeyConstraint('id')
     )
     op.create_table('quiz_attempts',
