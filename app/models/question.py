3411538c293c75b2053a58f688680848c860522f
shakgill786 <sgillani1@gmail.com>
2025-06-12 10:19:13 -0400

    ✅ Fixed FK issue + clean migration

--- app/models/question.py
+++ app/models/question.py
@@ -3,14 +3,14 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Question(db.Model):
     __tablename__ = "questions"
 
-    if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+    #if environment == "production":
+        #__table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     question_text = db.Column(db.String(255), nullable=False)
     options = db.Column(db.PickleType, nullable=False)
     answer = db.Column(db.String(100), nullable=False)
-    quiz_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("quizzes.id")), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quizzes.id"), nullable=False)
 
     quiz = db.relationship("Quiz", back_populates="questions")
 
